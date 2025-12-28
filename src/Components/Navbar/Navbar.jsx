5a9ad9646e6cd8c8e9ba7e1c678fceadb5f4d7e3
Ishann Ojhaa <ishannojhaa@gmail.com>
2025-12-28 13:03:00 +0545

    CreateBlog Done Successfully

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import './Navbar.css'
+import { useNavigate } from 'react-router-dom'
 const NavBar = () => {
+  const navigate =  useNavigate()
   return (
     <>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;600&display=swap" rel="stylesheet"></link>
+    
 <nav className="navbar">
         <div className="logo">
             <a href="/">  C<span>MS</span></a>
@@ -13,9 +16,9 @@ const NavBar = () => {
         <ul className="nav-links">
             <li><a href="/">Home</a></li>
             <li><a href="/about">About</a></li>
-            <li><a href="/createblog">Create</a></li>
-            <li><a href="#">Portfolio</a></li>
-            <li><a href="#">Contact</a></li>
+            <li><a onClick={()=> navigate('/createblog')}>Create</a></li>
+            <li><a href="/portfolio">Portfolio</a></li>
+            <li><a href="/contact">Contact</a></li>
         </ul>
         
         
--- src/Pages/CreateBlog/CreateBlog.jsx
+++ src/Pages/CreateBlog/CreateBlog.jsx
@@ -1,8 +1,65 @@
-import React from 'react'
+import React, { useState } from 'react'
+import './CreateBlog.css'
+import NavBar from '../../Components/Navbar/Navbar'
+import axios from 'axios'
+import { useNavigate } from 'react-router-dom'
 
 const CreateBlog = () => {
+  const navigate = useNavigate()
+  const [Title,setTitle] = useState("")
+  const [SubTitle,setSubTitle] = useState ("")
+  const [Description,setDescription] =useState ("")
+
+
+const createblog = async (e)=>{
+  e.preventDefault()
+  const data = {
+    Title : Title,         //backend_le_liyeko = State_Name
+    SubTitle : SubTitle,
+    Description : Description
+  }
+    const response =  await axios.post("http://localhost:3000/createblogs",data)
+  if(response.status == 200){
+    alert(response.data.message)
+    navigate("/")
+  }
+  else{
+    alert("Something Went Wrong")
+  }
+
+}
   return (
-    <div>CreateBlog</div>
+    <>
+    <NavBar/>
+ <div className="create-blog-container">
+      <div className="form-card">
+        <header className="form-header">
+            <h2>Get Started</h2>
+            <h1>Post Your Content </h1>
+            <p>Join our community today. It only takes a minute to set up your profile and start exploring.</p>
+        </header>
+
+        <form onSubmit={createblog} >
+            <div className="form-group">
+                <label htmlFor="Title">Title</label>
+                <input type="text" id="Title" placeholder="Enter Blog title" required onChange={(e)=>setTitle(e.target.value)}/>
+            </div>
+
+            <div className="form-group">
+                <label htmlFor="SubTitle">SubTitle</label>
+                <input type="text" id="SubTitle" placeholder="Enter Blog Subtitle (optional)" onChange={(e)=>setSubTitle(e.target.value)}/>
+            </div>
+
+            <div className="form-group">
+                <label htmlFor="Description"> Enter Your Blog in Detail </label>
+                <textarea id="Description" placeholder="Tell us about your Blog..." onChange={(e)=>setDescription(e.target.value)}></textarea>
+            </div>
+
+            <button type="submit" className="submit-btn"> Submit </button>
+        </form>
+    </div>
+    </div>
+    </>
   )
 }
 
